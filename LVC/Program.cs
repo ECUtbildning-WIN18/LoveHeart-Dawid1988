712ef4a357bd2dd69fd1a097ac7a4b0a4de06055
Dawid <dawid_owizyc@hotmail.com>
2018-10-08 21:07:51 +0200

    Add project files.

---
+++ LVC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
